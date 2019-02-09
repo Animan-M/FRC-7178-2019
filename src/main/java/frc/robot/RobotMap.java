e6820a498380fe8cabe6c758ffd3e8c2f703233b
Animan Manchanda <35748547+Routerz007@users.noreply.github.com>
2019-02-09 14:56:45 -0500

    Remove Astricks from Imports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -6,7 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Solenoid;
 
 /**
